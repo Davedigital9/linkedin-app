941a7ff29e189323000d42a7ec1b79c13586b8a6
David <151574925+Davedigital9@users.noreply.github.com>
2024-09-24 07:59:56 +0100

    created the header.js and home.js files in the componnts folder. Edited the App.js files: In React Router v6, components rendered inside a route need to be passed via the element prop. So,I wrapped the Header and Home components inside a fragment (<>...</>) and passed them via the element prop to the route I installed the styled-components inside the components folder to get rid of the styled componets not found error.

---
+++ src/components/Header.js
@@ -0,0 +1,33 @@
+import styled from 'styled-components';
+
+const Header = (props) => {
+    return (
+        <Container>
+            <Content>
+                Header
+            </Content>
+        </Container>
+    );
+};
+
+const Container = styled.div`
+    background-color: white;
+    border-bottom: 1px solid rgb(0, 0, 0, 0.8);
+    left : 0;
+    padding: 0 24px;
+    position: fixed;
+    top: 0;
+    width: 100w;
+    z-index: 100;
+`;
+
+const Content = styled.div`
+    display:flex;
+    align-items: center;
+    margin: 0 auto;
+    min-height: 100w;
+    max-width: 1128px;
+`;
+
+
+export default Header;
