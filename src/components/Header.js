b19e36ef98856b2a3a1fa70d479882f3f84862a4
David <151574925+Davedigital9@users.noreply.github.com>
2024-09-24 12:22:11 +0100

    Created the User menu and work menu.  The User menu is used to manage user accounts, and the Work menu is used to manage work orders. Added a drop down on the user icon for the SignOut function. The SignOut styling was moved above the User due to functionality issues. I put the "Me" text with the down icon inside the span tag in line 60 of Header.js

--- src/components/Header.js
+++ src/components/Header.js
@@ -54,6 +54,29 @@ const Header = (props) => {
                             </a>
                         </NavList>
 
+                        <User>
+                            <a>
+                                <img src="/images/user.svg" alt="" />
+                                <span>Me 
+                                    <img src="/images/down-icon.svg" alt="" />
+                                </span>
+                                
+                            </a>
+
+                            <SignOut>
+                                <a>Sign Out</a>
+                            </SignOut>
+                        </User>
+
+                        <Work>
+                            <a>
+                                <img src="/images/nav-work.svg" alt="" />
+                                <span> Work
+                                <img src="/images/down-icon.svg" alt="" />
+                                </span>
+                            </a>
+                        </Work>
+
                     </NavListWrap>
                 </Nav>
             </Content>
@@ -194,5 +217,47 @@ const NavList = styled.li`
   }
 `;
 
+const SignOut = styled.div`
+  position: absolute;
+  top: 45px;
+  background: white;
+  border-radius: 0 0 5px 5px;
+  width: 100px;
+  height: 40px;
+  font-size: 16px;
+  transition-duration: 167ms;
+  text-align: center;
+  display: none;
+`;
+
+const User = styled(NavList)`
+  a > svg {
+    width: 25px;
+    border-radius: 50%;
+  }
+
+  a > img {
+    width: 25px;
+    height: 25px;
+    border-radius: 50%;
+  }
+
+  span {
+    display: flex;
+    align-items: center;
+  }
+
+  &:hover {
+    ${SignOut} {
+      align-items: center;
+      display: flex;
+      justify-content: center;
+    }
+  }
+`;
+
+const Work = styled(User)`
+  border-left: 1px solid rgba(0, 0, 0, 0.08);
+`;
 
 export default Header;
