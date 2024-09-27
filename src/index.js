b4d5a2d4d8d4e9077a02666a73d78a541f0ddf5b
David <151574925+Davedigital9@users.noreply.github.com>
2024-09-27 22:09:16 +0100

    Installed react-redux with "npm install react-redux" and redux with "npm install redux" all inthe terminal and in the project directory. I imported the Provider from react-redux and store from ./store. Created a new folder, reducers, in the src folder and created an index.js file in it. A new file, userReducer.js was created  in the reducers folder. In this file, we created a reducer function that returns the state. Created the store folder in src. In index.js under reducers, import { userReducer } from "./userReducer"; was changed to import userReducer from "./userReducer"; This is because If a module has a default export, you don't need to use curly braces when importing

--- src/index.js
+++ src/index.js
@@ -1,14 +1,21 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import ReactDOM from 'react-dom/client'; // Ensure you're using the createRoot API
 import './index.css';
 import App from './App';
+import { Provider } from 'react-redux'; // Import the Provider from react-redux
+import store from './store'; // Import the Redux store
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
+
+// Render your app wrapped in the Redux Provider
 root.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}> {/* Wrapping the app with the Redux Provider */}
+      <App />
+    </Provider>
   </React.StrictMode>
 );
+
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
