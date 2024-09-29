519f609be5822ae4105a023a9fb2e61506a08fc1
David <151574925+Davedigital9@users.noreply.github.com>
2024-09-29 09:13:38 +0100

    Installed the react-router-dom and styled-component package in the project

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,8 @@
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import './App.css';
-import Header from './components/Header';
-import Home from './components/Home';
-import Login from './components/Login';
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import "./App.css";
+import Header from "./components/Header";
+import Home from "./components/Home";
+import Login from "./components/Login";
 
 function App() {
   return (
@@ -10,16 +10,19 @@ function App() {
       <Router>
         <Routes>
           <Route path="/" element={<Login />} />
-          <Route path="/home" element={
-            <>
-            <Header />
-            <Home />
-            </>
-            } />
+          <Route
+            path="/home"
+            element={
+              <>
+                <Header />
+                <Home />
+              </>
+            }
+          />
         </Routes>
       </Router>
     </div>
   );
 }
 
-export default App
+export default App;
